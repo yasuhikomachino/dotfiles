948fc6e9a35971593148758a7767ce8b257e1c18
Yasuhiko Machino <yasuhikomachino@gmail.com>
2024-11-03 22:03:33 +0900

    chore(wezterm): some tweaks

--- osx/wezterm/wezterm.lua
+++ osx/wezterm/wezterm.lua
@@ -1,25 +1,77 @@
 local wezterm = require("wezterm")
 
-local config = wezterm.config_builder()
-
-config.automatically_reload_config = false
-config.colors = {
-	background = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
+return {
+	automatically_reload_config = false,
+	colors = {
+		background = "#011423",
+		selection_bg = "#033259",
+		selection_fg = "#CBE0F0",
+	},
+	audible_bell = "Disabled",
+	scrollback_lines = 4096,
+	use_ime = true,
+	window_padding = {
+		left = 5,
+		right = 5,
+		top = 5,
+		bottom = 5,
+	},
+	font = wezterm.font("Hack Nerd Font Mono"),
+	font_size = 16,
+	keys = {
+		{
+			key = "Enter",
+			mods = "SUPER",
+			action = wezterm.action({
+				SplitVertical = { domain = "CurrentPaneDomain" },
+			}),
+		},
+		{
+			key = "Enter",
+			mods = "SUPER|SHIFT",
+			action = wezterm.action({
+				SplitHorizontal = { domain = "CurrentPaneDomain" },
+			}),
+		},
+		{
+			key = "h",
+			mods = "SUPER",
+			action = wezterm.action({ ActivatePaneDirection = "Left" }),
+		},
+		{
+			key = "l",
+			mods = "SUPER",
+			action = wezterm.action({ ActivatePaneDirection = "Right" }),
+		},
+		{
+			key = "k",
+			mods = "SUPER",
+			action = wezterm.action({ ActivatePaneDirection = "Up" }),
+		},
+		{
+			key = "j",
+			mods = "SUPER",
+			action = wezterm.action({ ActivatePaneDirection = "Down" }),
+		},
+		{
+			key = "u",
+			mods = "SUPER",
+			action = wezterm.action({ ScrollByPage = -0.25 }),
+		},
+		{
+			key = "d",
+			mods = "SUPER",
+			action = wezterm.action({ ScrollByPage = 0.25 }),
+		},
+		-- tab
+		{ key = "1", mods = "SUPER", action = wezterm.action({ ActivateTab = 0 }) },
+		{ key = "2", mods = "SUPER", action = wezterm.action({ ActivateTab = 1 }) },
+		{ key = "3", mods = "SUPER", action = wezterm.action({ ActivateTab = 2 }) },
+		{ key = "4", mods = "SUPER", action = wezterm.action({ ActivateTab = 3 }) },
+		{ key = "5", mods = "SUPER", action = wezterm.action({ ActivateTab = 4 }) },
+		{ key = "6", mods = "SUPER", action = wezterm.action({ ActivateTab = 5 }) },
+		{ key = "7", mods = "SUPER", action = wezterm.action({ ActivateTab = 6 }) },
+		{ key = "8", mods = "SUPER", action = wezterm.action({ ActivateTab = 7 }) },
+		{ key = "9", mods = "SUPER", action = wezterm.action({ ActivateTab = 8 }) },
+	},
 }
-config.audible_bell = "Disabled"
-config.scrollback_lines = 4096
-config.use_ime = true
-config.window_padding = {
-	left = 5,
-	right = 5,
-	top = 5,
-	bottom = 5,
-}
-config.font = wezterm.font("Hack Nerd Font Mono")
-config.font_size = 16
-
-return config
