78cd737d24d99afae21039abbc9a563936b062b3
Yasuhiko Machino <yasuhikomachino@gmail.com>
2024-12-07 14:52:21 +0900

    chore(nvim): some tweaks

--- osx/nvim/lua/machi/core/options.lua
+++ osx/nvim/lua/machi/core/options.lua
@@ -1,9 +1,12 @@
-vim.cmd("let g:netrw_liststyle=3")
-
 local opt = vim.opt
 
+-- basics
 opt.relativenumber = true
 opt.number = true
+opt.cursorline = true
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
 
 -- tabs & indentation
 opt.tabstop = 2
@@ -16,18 +19,15 @@ opt.wrap = false
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assume you want case-sensitive
 
-opt.cursorline = true
-
-opt.termguicolors = true
-opt.background = "dark"
-opt.signcolumn = "yes"
-
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, ent of line or insert mode start position
 
 -- clipboard
+opt.clipboard:append({ "unnamedplus" })
+
+-- window opening direction
 opt.splitright = true
 opt.splitbelow = true
 
--- Ignore automatic formatting and save
+-- ignore automatic formatting and save
 vim.api.nvim_create_user_command("W", "noautocmd w", {})
