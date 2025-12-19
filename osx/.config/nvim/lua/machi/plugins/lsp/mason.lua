1b183d1c2ec400806f0a97b14b8f672d86bacb86
Yasuhiko Machino <yasuhikomachino@gmail.com>
2025-12-20 00:08:36 +0900

    fix(nvim): migrate deprecated setup_handlers to handlers in mason-lspconfig

--- osx/.config/nvim/lua/machi/plugins/lsp/mason.lua
+++ osx/.config/nvim/lua/machi/plugins/lsp/mason.lua
@@ -24,6 +24,15 @@ return {
       },
     })
 
+    -- import lspconfig plugin
+    local lspconfig = require("lspconfig")
+
+    -- import cmp-nvim-lsp plugin
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+    -- used to enable autocompletion (assign to every lsp server config)
+    local capabilities = cmp_nvim_lsp.default_capabilities()
+
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
@@ -45,6 +54,117 @@ return {
         "terraformls",
         "dockerls",
         "docker_compose_language_service",
+      },
+      -- handlers for setting up LSP servers
+      handlers = {
+        -- default handler for installed servers
+        function(server_name)
+          lspconfig[server_name].setup({
+            capabilities = capabilities,
+          })
+        end,
+        ["svelte"] = function()
+          lspconfig["svelte"].setup({
+            capabilities = capabilities,
+            on_attach = function(client, bufnr)
+              vim.api.nvim_create_autocmd("BufWritePost", {
+                pattern = { "*.js", "*.ts" },
+                callback = function(ctx)
+                  client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+                end,
+              })
+            end,
+          })
+        end,
+        ["graphql"] = function()
+          lspconfig["graphql"].setup({
+            capabilities = capabilities,
+            filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+          })
+        end,
+        ["html"] = function()
+          lspconfig["html"].setup({
+            capabilities = capabilities,
+            filetypes = { "html", "blade", "htmldjango" },
+            init_options = {
+              configurationSection = { "html", "css", "javascript" },
+              embeddedLanguages = {
+                css = true,
+                javascript = true,
+              },
+              provideFormatter = true,
+            },
+          })
+        end,
+        ["cssls"] = function()
+          lspconfig["cssls"].setup({
+            capabilities = capabilities,
+            filetypes = { "css", "scss", "less" },
+            settings = {
+              css = {
+                validate = true,
+                lint = {
+                  unknownAtRules = "ignore",
+                },
+              },
+              scss = {
+                validate = true,
+                lint = {
+                  unknownAtRules = "ignore",
+                },
+              },
+              less = {
+                validate = true,
+                lint = {
+                  unknownAtRules = "ignore",
+                },
+              },
+            },
+          })
+        end,
+        ["emmet_ls"] = function()
+          lspconfig["emmet_ls"].setup({
+            capabilities = capabilities,
+            filetypes = {
+              "html",
+              "blade",
+              "htmldjango",
+              "typescriptreact",
+              "javascriptreact",
+              "css",
+              "sass",
+              "scss",
+              "less",
+              "svelte",
+            },
+          })
+        end,
+        ["ts_ls"] = function()
+          lspconfig["ts_ls"].setup({
+            capabilities = capabilities,
+            filetypes = {
+              "javascript",
+              "javascriptreact",
+              "typescript",
+              "typescriptreact",
+            },
+          })
+        end,
+        ["lua_ls"] = function()
+          lspconfig["lua_ls"].setup({
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                diagnostics = {
+                  globals = { "vim" },
+                },
+                completion = {
+                  callSnippet = "Replace",
+                },
+              },
+            },
+          })
+        end,
       },
     })
 
