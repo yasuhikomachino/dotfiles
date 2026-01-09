53ac4c73cc9142631d0fb629475a091a2306f202
Yasuhiko Machino <yasuhikomachino@gmail.com>
2026-01-09 13:10:55 +0900

    feat(nvim): enhance telescope with trouble integration and resume

--- osx/.config/nvim/lua/machi/plugins/telescope.lua
+++ osx/.config/nvim/lua/machi/plugins/telescope.lua
@@ -11,14 +11,11 @@ return {
     local telescope = require("telescope")
     local actions = require("telescope.actions")
     local transform_mod = require("telescope.actions.mt").transform_mod
-
-    --    local trouble = require("trouble")
-    --    local trouble_telescope = require("trouble.sources.telescope")
-
-    -- or create your custom action
     local custom_actions = transform_mod({
       open_trouble_qflist = function(prompt_bufnr)
-        trouble.toggle("quickfix")
+        vim.schedule(function()
+          require("trouble").open("quickfix")
+        end)
       end,
     })
 
@@ -29,8 +26,11 @@ return {
           i = {
             ["<C-k>"] = actions.move_selection_previous, -- move to prev result
             ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-            --            ["<C-t>"] = trouble_telescope.open,
+            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist, -- send selected to quickfix
+            ["<M-q>"] = actions.send_to_qflist + custom_actions.open_trouble_qflist, -- send all to quickfix
+            ["<C-t>"] = function(prompt_bufnr)
+              require("trouble.sources.telescope").open(prompt_bufnr)
+            end, -- open in trouble directly
           },
         },
       },
@@ -47,5 +47,6 @@ return {
     keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
     keymap.set("n", "<leader>fb", "<cmd>Telescope buffers hidden=true<cr>", { desc = "Fuzzy find buffers" })
     keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+    keymap.set("n", "<leader>fR", "<cmd>Telescope resume<cr>", { desc = "Resume last search" })
   end,
 }
