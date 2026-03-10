a4bd3f0087618159add9a25bd79e799928e84f29
Yasuhiko Machino <yasuhikomachino@gmail.com>
2026-03-10 21:06:17 +0900

    feat(wezterm): enable title bar in window decorations

--- osx/.config/wezterm/wezterm.lua
+++ osx/.config/wezterm/wezterm.lua
@@ -7,7 +7,7 @@ return {
 	automatically_reload_config = true,
 	window_background_opacity = 1.00,
 	macos_window_background_blur = 0,
-	window_decorations = "RESIZE",
+	window_decorations = "TITLE|RESIZE",
 	use_fancy_tab_bar = false,
 	hide_tab_bar_if_only_one_tab = true,
 	show_new_tab_button_in_tab_bar = false,
