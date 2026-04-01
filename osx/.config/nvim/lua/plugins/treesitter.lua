1217d89b7ddbcff0d45f29f4f60fa86f8ed8eb39
Yasuhiko Machino <yasuhikomachino@gmail.com>
2026-04-01 10:35:34 +0900

    fix(nvim): migrate to AstroNvim v6 and resolve Neovim 0.12.0 treesitter errors
    Switch nvim-treesitter / nvim-treesitter-textobjects to main branch
    and update aerial.nvim to v3.

--- osx/.config/nvim/lua/plugins/treesitter.lua
+++ osx/.config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,5 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
--- Customize Treesitter
-
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      -- add more arguments for adding more treesitter parsers
-    },
-  },
+  { "nvim-treesitter/nvim-treesitter", branch = "main" },
+  { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
 }
